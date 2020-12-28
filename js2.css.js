b71963bd1505cffedc0d68eacff43fe728d8cf31
Olgakuzina <57990663+Olgakuzina@users.noreply.github.com>
2020-12-28 10:18:58 +0200

    Add files via upload

---
+++ js2.css.js
@@ -0,0 +1,11 @@
+function f(){
+document.getElementById("pic1").src="2.gif";
+document.getElementById("body1").style.backgroundColor="blue";
+document.getElementById("body1").style.backgroundColor="yellow";
+}
+function g(){if(document.getElementById("sl1").selectedIndex==0)
+document.getElementById("body1").style.backgroundColor="green";
+if(document.getElementById("sl1").selectedIndex==1)document.getElementById("body1").style.backgroundColor="red";
+if(document.getElementById("sl1").selectedIndex==2)
+document.getElementById("body1").style.backgroundColor="pink";}
+function rr(){document.getElementById("area1").value="Привет, Мир!!!";}
